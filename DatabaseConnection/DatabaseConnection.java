d8a3a55cc780332b76b45ede89d7b0222485c68c
Kmayno <carminenigro4@gmail.com>
2025-06-29 01:05:27 +0200

    ao

--- DatabaseConnection/DatabaseConnection.java
+++ DatabaseConnection/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
 public class DatabaseConnection {
     private static final String url = "jdbc:mysql://localhost:3306/VendingMachineDB";
     private static final String user="root";
-    private static final String password="root";
+    private static final String password="1234";
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(url,user,password);
